8fc62d6354ba1e22842b3dc1a8f20873d2dbccf5
MelissaN <majajachiu@yahoo.com>
2018-08-27 22:22:50 +0000

    file 3- export all API data to JSON

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+# Request from API; Return TODO list progress of all employees
+# Export this data to JSON
+import json
+import requests
+
+
+def all_to_json():
+    """return API data"""
+    USERS = []
+    userss = requests.get("http://jsonplaceholder.typicode.com/users")
+    for u in userss.json():
+        USERS.append((u.get('id'), u.get('username')))
+    TASK_STATUS_TITLE = []
+    todos = requests.get("http://jsonplaceholder.typicode.com/todos")
+    for t in todos.json():
+        TASK_STATUS_TITLE.append((t.get('userId'), t.get('completed'), t.get('title')))
+
+    """export to json"""
+    data = dict()
+    for u in USERS:
+        t = []
+        for task in TASK_STATUS_TITLE:
+            if task[0] == u[0]:
+                t.append({"task": task[2], "completed": task[1], "username": u[1]})
+        data[str(u[0])] = t
+    filename = "todo_all_employees.json"
+    with open(filename, "w") as f:
+        json.dump(data, f)
+
+
+if __name__ == "__main__":
+    all_to_json()
