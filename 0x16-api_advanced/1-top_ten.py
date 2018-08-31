eef63ca6523aed39899b03dfa189a659bd6686d0
MelissaN <majajachiu@yahoo.com>
2018-08-31 00:41:27 +0000

    1- add script to find top_ten subreddit titles

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Query Reddit API for titles of top ten posts of a given subreddit
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+        return number of subscribers for a given subreddit
+        return 0 if invalid subreddit given
+    """
+    # get user agent
+    # https://stackoverflow.com/questions/10606133/ -->
+    # sending-user-agent-using-requests-library-in-python
+    headers = requests.utils.default_headers()
+    headers.update({'User-Agent': 'My User Agent 1.0'})
+
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    r = requests.get(url, headers=headers).json()
+    top_ten = r.get('data', {}).get('children', [])
+    if not top_ten:
+        print(None)
+    for t in top_ten:
+        print(t.get('data').get('title'))
